48a8fb2e44e9361149adab94003c996686f51925
Chizim Chinuru <garrohat@yahoo.co.uk>
2022-05-11 19:05:47 +0100

    added some changes

--- hello-world.go
+++ hello-world.go
@@ -126,4 +126,11 @@ func leapYearChecker(year int) {
 		fmt.Printf("%d is not a leap year \n", year)
 	}
 }
+func leapYearChecker1(year int) {
+	if year%4 == 0 && year%100 != 0 || year%400 == 0 {
+		fmt.Printf("%d is a leap year \n", year)
+	} else {
+		fmt.Printf("%d is not a leap year \n", year)
+	}
+}
 
